b35722ac5d9495998388048e2820e3859d731254
Patrick Kranz <patrick-kranz@live.de>
2022-01-01 10:02:44 +0100

    cleanup repo

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import App from './main/App.svelte';
-const app = new App({
-    target: document.body,
-    props: {
-        name: 'world'
-    }
-});
-export default app;
-//# sourceMappingURL=main.js.map
